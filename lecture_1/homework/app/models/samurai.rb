8a0d3a5c1e8695cd4e6060d56dabb62113d5f030
katguz3485 <katarzyna.guzik01@gmail.com>
2019-04-29 20:22:48 +0200

    added samurai model validations and scopes

--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -1,3 +1,11 @@
 class Samurai < ApplicationRecord
-  belong_to :clan
+  belongs_to :clan
+  validates :name, presence: true, length: {maximum: 100}
+  validates :shield_quality, presence: true, numericality: {greater_than: 0, less_than: 1000}
+  validates :number_of_battles, presence: true
+  validates :join_date, presence: true
+
+  scope :dead, -> {where('death_date is NOT_NULL')}
+  scope :alive, -> {where('death_date is NULL')}
+
 end
