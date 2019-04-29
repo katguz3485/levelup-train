ffae6b64f1cb7e9fc554c36e37e2c27350514be8
katguz3485 <katarzyna.guzik01@gmail.com>
2019-04-29 21:46:55 +0200

    controller actions and routes added

--- lecture_1/homework/app/controllers/samurais_controller.rb
+++ lecture_1/homework/app/controllers/samurais_controller.rb
@@ -1,32 +1,36 @@
 class SamuraisController < ApplicationController
 
   def create
-
+    samurai = clan.samurais.create!(samourai_params)
+    render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
   end
 
   def index
+
 
   end
 
 
   def show
-
+    render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
   end
 
 
   def update
-
+    clan.samurai.update!(samurai_params)
+    render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
   end
 
   def delete
-
+    samurai.destroy!
+    head 204
   end
 
   private
 
 
   def samourai
-    @samurai = clan.samourais.find(params[:id])
+    @samurai ||= clan.samurais.find(params[:id])
   end
 
 
