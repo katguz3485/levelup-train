30d68a057233a6a20c5a501292193ed1855a19da
katguz3485 <katarzyna.guzik01@gmail.com>
2019-04-29 23:04:12 +0200

    works but still need improvements

--- lecture_1/homework/app/controllers/clans_controller.rb
+++ lecture_1/homework/app/controllers/clans_controller.rb
@@ -1,18 +1,20 @@
-class ClansController < ApplicationController
+module Api
+  class ClansController < ApplicationController
 
-  def index
-    clans = Clan.all
-    render json: clans.to_json(only: %w[id name])
-  end
+    def index
+      clans = Clan.all
+      render json: clans.to_json(only: %w[id name])
+    end
 
-  def create
-    clan = Clan.create(clan_params)
-    render :json, clan.to_json(only: %w[id name]), status: 201
-  end
+    def create
+      clan = Clan.create(clan_params)
+      render :json, clan.to_json(only: %w[id name]), status: 201
+    end
 
-  private
+    private
 
-  def clan_params
-    params.require(:clan).permit(:name)
+    def clan_params
+      params.require(:clan).permit(:name)
+    end
   end
 end
--- lecture_1/homework/app/controllers/samurais_controller.rb
+++ lecture_1/homework/app/controllers/samurais_controller.rb
@@ -1,46 +1,55 @@
-class SamuraisController < ApplicationController
+module Api
+  class SamuraisController < ApplicationController
 
-  def create
-    samurai = clan.samurais.create!(samourai_params)
-    render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
+    def create
+      samurai = clan.samurais.create!(samourai_params)
+      render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
+    end
+
+    def index
+      if params[:dead]
+        samurais = clan.samurais.dead
+      elsif params[:alive]
+        samurais = clan.samurais.alive
+      else
+        samurais = clan.samurais
+      end
+
+      render json: samurais.to_json(only: %w[name shield_quality number_of_battles join_date])
+    end
+
+
+    def show
+      render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
+    end
+
+
+    def update
+      clan.samurai.update!(samurai_params)
+      render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
+    end
+
+    def delete
+      samurai.destroy!
+      head 204
+    end
+
+    private
+
+
+    def samourai
+      @samurai ||= clan.samurais.find(params[:id])
+    end
+
+
+    def clan
+      @clan ||= Clan.find(params[:clan_id])
+    end
+
+    def samurai_params
+      params.permit(:name, :shield_quality, :number_of_battles, :join_date, :death_date)
+
+    end
+
   end
-
-  def index
-
-
-  end
-
-
-  def show
-    render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
-  end
-
-
-  def update
-    clan.samurai.update!(samurai_params)
-    render json: samurai.to_json(only: %w[name shield_quality number_of_battles join_date])
-  end
-
-  def delete
-    samurai.destroy!
-    head 204
-  end
-
-  private
-
-
-  def samourai
-    @samurai ||= clan.samurais.find(params[:id])
-  end
-
-
-  def clan
-    @clan ||= Clan.find(params[:clan_id])
-  end
-
-  def samurai_params
-    params.permit(:name, :shield_quality, :number_of_battles, :join_date, :death_date)
-
-  end
-
 end
