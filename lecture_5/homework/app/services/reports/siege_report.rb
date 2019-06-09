07c66fc97807ab3d52180ae868adb4313e93b8df
katguz3485 <katarzyna.guzik01@gmail.com>
2019-06-09 19:05:40 +0200

    code cleanup

--- lecture_5/homework/app/services/reports/siege_report.rb
+++ lecture_5/homework/app/services/reports/siege_report.rb
@@ -9,34 +9,33 @@ module Reports
     end
 
     def call
-      if @building.warriors.alive.present?
-        siege_ability
-        # @building.update!(siege_ability: siege_ability)
-      else
-        0
-      end
+      check_warriors_presence
     end
 
     private
+
+    def check_warriors_presence
+      building.warriors.alive.present? ? siege_ability : 0
+    end
 
     def siege_ability
       granary / consume_ratio
     end
 
     def granary
-      @building.granary
+      building.granary
     end
 
     def infantry_warriors_number
-      Buildings::BuildingsQuery.count_infantry_warriors(building: @building)
+      Buildings::BuildingsQuery.count_infantry_warriors(building: building)
     end
 
     def calvary_warriors_number
-      Buildings::BuildingsQuery.count_calvary_warriors(building: @building)
+      Buildings::BuildingsQuery.count_calvary_warriors(building: building)
     end
 
     def consume_ratio
-      10 + infantry_warriors_number + (2 * calvary_warriors_number)
+      10 + infantry_warriors_number + 2 * calvary_warriors_number
     end
   end
 end
